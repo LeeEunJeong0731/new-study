74c57ba11b4e6f45c440efe970ffc4cf6a2f7366
eunjeong <eunjeong990731@gmail.com>
2023-11-05 20:01:12 +0900

    if문을이용하여변수가 짝수 홀수 판별

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-
-for(let i=1 ; i<10; i++){
-   console.log (i)
+const num = 2
+if ( num % 2 == 0 ){
+  return console.log("짝수")
+}else{
+  return console.log("홀수")
 }
+
+// %는 나머지 연산자!
--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-for i in range(10):
-    print(str(i+1)) 
+num = 2
+
+if num % 2 == 0 : 
+    print ("짝수")
+else :
+    print("홀수")
+# * 나머지 연산자 % 그리고 파이썬은 === 이 아니라 ==으로 계산이 됨
