a017c4f74b2447d00b9d3670ce83f806d1718d82
eunjeong <eunjeong990731@gmail.com>
2023-11-26 22:05:30 +0900

    :memo:
    +,-,*,/  연산자를 사용해 간단한 산술 연산을 하는 코드를 작성

---
+++ q19.js
@@ -0,0 +1,8 @@
+//* +,-,*,/  연산자를 사용해 간단한 산술 연산을 하는 코드를 작성
+const a = 5;
+const b = 3;
+
+console.log(a + b); // 더하기
+console.log(a - b); // 빼기
+console.log(a * b); // 곱하기
+console.log(a / b); // 나누기
---
+++ q19.py
@@ -0,0 +1,7 @@
+a = 5
+b = 3
+
+print(a + b) # 더하기
+print(a - b) # 빼기 
+print(a * b) # 곱하기
+print(a / b) # 나누기 
