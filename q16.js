e5bd0898be7ff8809e84dfd328dc034bdf84c8f2
eunjeong <eunjeong990731@gmail.com>
2023-11-26 21:57:29 +0900

    :memo:
    16.fypeof 연산자를 사용해 변수의 타입을 확인하는 예제 코드를 작성해보세요.

---
+++ q16.js
@@ -0,0 +1,9 @@
+//* 16.fypeof 연산자를 사용해 변수의 타입을 확인하는 예제 코드를 작성해보세요.
+
+const a = 'hello';
+
+if (typeof a === 'string') {
+  console.log('문자열입니다.');
+} else {
+  console.log('문자열로 부탁두려용');
+}
---
+++ q16.py
@@ -0,0 +1,7 @@
+num = 2
+
+if type(num) :
+    num
+    print("숫자입니다.")
+else : 
+    print ("숫자열로 부탁드려용")
