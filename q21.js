b0057f7722db570f8796a410cfdf36a3750eddd4
eunjeong <eunjeong990731@gmail.com>
2023-11-26 22:12:44 +0900

    :memo:
    21.x가 y보다 큰지 확인하는 조건식을 작성

---
+++ q21.js
@@ -0,0 +1,10 @@
+//* 21.x가 y보다 큰지 확인하는 조건식을 작성
+
+const x = 5;
+const y = 3;
+
+if (x > y) {
+  console.log('x가 y보다 큽니다.');
+} else {
+  console.log('x가 y보다 작거나 같습니다.');
+}
---
+++ q21.py
@@ -0,0 +1,7 @@
+x = 5
+y = 3
+
+if x > y:
+    print("x가 y보다 큽니다.")
+else:
+    print("x가 y보다 작거나 같습니다.")
