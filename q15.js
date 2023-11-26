73a26b23d6e49cd905117850e690f3cacf042ca8
eunjeong <eunjeong990731@gmail.com>
2023-11-26 21:55:25 +0900

    :memo:
    15.조건문에서 truthy와 falsy라는 개념이 무엇인가요? 각각의 예를 들어보세요

---
+++ q15.js
@@ -0,0 +1,20 @@
+// * 15.조건문에서 truthy와 falsy라는 개념이 무엇인가요? 각각의 예를 들어보세요
+
+// truthy 예제
+if (true) {
+  console.log('이 조건은 참입니다.');
+}
+
+// falsy 예제
+if (false) {
+  console.log('이 조건은 거짓이므로 실행되지 않습니다.');
+}
+/**
+ * `truthy` : 조건이 참으로 간주되는 값들이다. true 로 변환이 된다.
+
+ex : 숫자 1 , 문자열 hello
+
+`falsy` : 조건이 거짓으로 간주되는 값이다. false로 변환이 된다.
+
+ex : 숫자0 , 빈 문자열 “” ,null ,undefind, NaN 등이 있다.
+ */
