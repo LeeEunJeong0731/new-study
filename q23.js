33c0e5c3a35761227639f06b03dac11b8d0e3b3d
eunjeong <eunjeong990731@gmail.com>
2023-11-29 20:38:43 +0900

    :memo:
    23. x를 y로 나눈 몫을 구하는 코드를 작성

---
+++ q23.js
@@ -0,0 +1,7 @@
+//* 23. x를 y로 나눈 몫을 구하는 코드를 작성
+const x = 20;
+const y = 3;
+
+const quotient = Math.floor(x / y);
+
+console.log(`x를 y로 나눈 몫은 ${quotient}입니다.`);
---
+++ q23.py
@@ -0,0 +1,6 @@
+x = 20
+y = 3
+
+quotient = x // y
+
+print(f"나눈 값은 {quotient}입니다.")
