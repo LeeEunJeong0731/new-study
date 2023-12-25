3c8f5b5c8769915fecd0a93ff34a6ec273cb6c83
ChaeYi <chaeyi990731@gmail.com>
2023-12-25 16:44:57 +0900

    스위치 문을 사용해 변수가 어떤 값인지 알수있다.

---
+++ q26.js
@@ -0,0 +1,16 @@
+// 26. switch 문을 사용해 변수가 어떤 값인지 분기하는 코드를 작성 하세요.
+let number = 2;
+
+switch (number) {
+  case 1:
+    console.log('숫자는 1입니다.');
+    break;
+  case 2:
+    console.log('숫자는 2입니다.');
+    break;
+  case 3:
+    console.log('숫자는 3입니다.');
+    break;
+  default:
+    console.log('해당하는 숫자가 없습니다.');
+}
